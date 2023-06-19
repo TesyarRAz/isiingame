4c67763790ce7c51e7360038259f54802c77eb9a
TesyarRAz <mohamadtesyar@gmail.com>
2023-06-19 15:44:49 +0700

    fix file manager for file upload

--- application/libraries/File_manager.php
+++ application/libraries/File_manager.php
@@ -1,61 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class User_model extends CI_Model {
-    public function userWhere($where)
+class File_manager
+{
+    private $instance;
+
+    public function __construct()
     {
-        return $this->db->where($where)->get('tbl_user')->row_array();
+        $this->instance = &get_instance();
     }
 
-    public function insert($data)
+    public function upload_file($name, $dir)
     {
-        return $this->db->insert('tbl_user', $data);
-    }
+        $config['upload_path']          =  $dir;
+        $config['allowed_types']        = '*';
+        $config['encrypt_name']         = true;
 
-    public function count_all()
-    {
-        return $this->db->count_all('tbl_user');
-    }
+        $this->instance->load->library('upload', $config);
 
-    public function update($where, $data)
-    {
-        return $this->db->where($where)->update('tbl_user', $data);
-    }
+        if ($this->instance->upload->do_upload($name)) {
+            return [true, $this->instance->upload->data()];
+        }
 
-    public function delete($where)
-    {
-        return $this->db->delete('tbl_user', $where);
-    }
-
-
-    public function page($limit, $page)
-    {
-        $this->db->limit($limit, $page);
-
-        return $this;
-    }
-    
-    public function latest()
-    {
-        $this->db->order_by('id_user', 'desc');
-
-        return $this;
-    }
-
-    public function where($where)
-    {
-        $this->db->where($where);
-
-        return $this;
-    }
-
-    public function all()
-    {
-        return $this->db->get('tbl_user')->result_array();
-    }
-
-    public function first_where($where)
-    {
-        return $this->db->where($where)->get('tbl_user')->row_array();
+        return [false, $this->instance->upload->display_errors()];
     }
 }
